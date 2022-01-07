a2f1f6b00a2cf15190913fd0ff4d18a171bb07ff
Duong-Dao <daoduong2809@gmail.com>
2022-01-07 20:36:31 +0700

    add router user

--- routes/v1/index.js
+++ routes/v1/index.js
@@ -1,5 +1,7 @@
-const Router = require("koa-router")
-const router = new Router()
-const posts = require('./posts')
-router.use("/posts", posts)
-module.exports = router.routes()
+const Router = require("koa-router");
+const router = new Router();
+const posts = require('./posts');
+const users = require('./users');
+router.use("/posts", posts);
+router.use("/users", users);
+module.exports = router.routes();
